0f8dcb89d6bc1230e2379cddaf06abdc952c21bf
candydasein <michaellorensiegel@gmail.com>
2018-10-10 16:25:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,23 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{rank: "queen",
+ 	 suit: "hearts",
+ 	 cardImage: "images/queen-of-hearts.png"
+	},
+	{rank: "queen",
+	 suit: "diamonds",
+	 cardImage: "images/queen-of-diamonds.png"
+	},
+	{rank: "king",
+	 suit: "hearts",
+	 cardImage: "images/king-of-hearts.png"
+	},
+	{rank: "king",
+	 suit: "diamonds",
+	 cardImage: "images/king-of-diamonds.png"
+	}
+	];
 var cardsInPlay = [];
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -9,8 +27,10 @@ var checkForMatch = function() {
 };
 
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped" + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped" + " " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
@@ -18,6 +38,8 @@ var flipCard = function (cardId) {
 
 flipCard(0);
 flipCard(2);
+
+
 
 
 
